c065246b90b7abadacd4779b0d274e643677c658
mmccjjj <dhuber88@sunrise.ch>
2024-06-24 16:49:31 +0200

    übung Vergleichsoperatoren

--- www/public/index.php
+++ www/public/index.php
@@ -12,7 +12,73 @@
 
   <?php
 
-  phpinfo();
+ /*  phpinfo(); */
+
+ $name= 'Dani';
+ $alter= 35;
+ $istStudent= true;
+ $geburtsjahr= '1988';
+ $berechnetesAlter= date("Y")- $geburtsjahr;
+ $zahl= "15";
+ $istWahr= true;
+ $string= "test";
+ $a= 5;
+ $b= 10;
+
+ $summe= $a+ $b;
+ $differenz= $b- $a;
+ $produkt= $a* $b;
+ $quotient= $b/ $a;
+ $totalMinuten= rand(60, 800);
+ $stunden= floor($totalMinuten/ 60);
+ $minuten= $totalMinuten% 60;
+
+ if ($istStudent== true){
+    $result= 'Ja';
+ } else{
+  $result= 'Nein';
+ }
+
+ echo '<p>Mein Name ist ' . $name . ' Ich bin ' . $alter . ' Jahre alt. Bin Ich Student? '  . $result . '</p>';
+
+ echo "<p>Mein Name ist $name Ich bin $alter Jahre alt. Bin Ich Student? $result</p>";
+
+ echo "<p>Mein Name ist $name Ich bin $berechnetesAlter Jahre alt und im Jahr $geburtsjahr geboren.</p>";
+
+ $zahlAlsInt= (int)$zahl;
+
+ $zahl= (string)$zahlAlsInt;
+
+ $istWahrAlsString= (string)$istWahr;
+
+ $istWahrAlsInteger= (int)$istWahr;
+
+ $zahlAlsBoolean= (bool)$zahl;
+
+ $stringAlsBoolean= (bool)$string;
+
+
+ echo gettype($zahl);
+ echo "<br>";
+ echo gettype($istWahrAlsInteger);
+ echo "<br>";
+ echo gettype($zahlAlsBoolean);
+ echo "<br>";
+ echo gettype($stringAlsBoolean);
+ echo "<br>";
+ echo gettype($istWahrAlsString). "<br>";
+ echo "Summe: $summe <br>";
+ echo "Differenz: $differenz <br>";
+ echo "Produkt: $produkt <br>";
+ echo "Quotient: $quotient <br>";
+
+ echo "minuten gesammt: $totalMinuten <br>";
+ echo "Stunden: $stunden <br>";
+ echo "Minuten: $minuten"
+ 
+
+
+
 
   ?>
 
